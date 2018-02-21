70329705753a981a01172f30bda7852197974c60
efron_licht <efron.python@gmail.com>
2018-02-21 13:06:19 -0800

    working using number_theory crate

--- euler_005/src/main.rs
+++ euler_005/src/main.rs
@@ -1,102 +1,14 @@
-use std::collections;
+
+extern crate number_theory;
+use number_theory::primes;
+
+///2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+///What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 fn main() {
-    println!("Hello, world!");
+    let p = primes::Primes::under(25);
+    let union = (2..21)
+        .map(|n| primes::factors(n, &p).unwrap())
+        .fold(primes::Factors::new(), |a, b| primes::union(&a, &b));
+    let lcm: u64 = union.iter().map(|(base, exp)| base.pow(*exp as u32)).product();
+    println!("{}", lcm)
 }
-
-type Counter = collections::HashMap<u64, u32>;
-type Primes = Vec<u64>;
-
-fn union(a: &Counter, b: &Counter) -> Counter {
-    let mut union = a.clone();
-    for (k, v) in b {
-         if !union.contains_key(k) {
-                union.insert(*k, *v);
-            } else {
-                let m = union[k];
-                union.insert(*k, m.max(*v));
-        }
-    }
-    union
-}
-
-
-fn intersection(a: &Counter, b: &Counter) -> Counter {
-    let mut intersection = collections::HashMap::new();
-    for (k, v) in a {
-        if b.contains_key(k) {
-            let min = *v.min(&b[k]);
-            intersection.insert(*k, min);
-        }
-    }
-    intersection
-}
-///gcd is the greatest common divisor.  that is, the largest d such that ad == m bd == n for some positive integers a, b
-fn gcd(m: u64, n: u64, primes: &Primes) -> Option<u64> {
-    if m == 0 || n == 0 {return None}
-    let intersection = intersection(&factors(n, primes)?, &factors(m, primes)?);
-    let mut gcd = 1;
-    for (p, exp) in intersection {
-        gcd *= p.pow(exp);
-    }
-    Some(gcd)
-}
-
-//lcm is the least common multiple; that is, the smallest q such that a*m == q, b*m == q for some positive integers a, b
-fn lcm(m: u64, n: u64, primes: &Primes) -> Option<u64> {
-    if m == 0 || n == 0 {
-        return Some(0)
-    }
-    let union = union(&factors(n, primes)?, &factors(m, primes)?);
-    let mut lcm = 1;
-    for (p, exp) in union.iter() {
-        lcm *= p.pow(*exp);
-    }
-    Some(lcm)
-}
-
-
-fn primes_under(max: u64) -> Primes {
-    let mut primes = Vec::new();
-    if max < 2 {
-        return primes    
-    }
-    primes.push(2);
-    let mut n = 3;
-
-
-    while n < max{
-        let mut isprime = true;
-        for p in &primes {
-            if n % p == 0 {
-                isprime = false;
-                break 
-            }
-        }
-        if isprime {
-            primes.push(n);
-        }
-
-        n +=2
-    }
-    primes
-}
-
-
-
-fn factors(n: u64, primes: &Primes) -> Option<Counter>{
-
-    let mut factors = collections::HashMap::new();
-    let mut n = n;
-
-    for p in primes {
-        while n % p == 0 {
-            *factors.entry(n).or_insert(0) += 1;
-            n /= p;
-        }
-        if p > &n {
-            return Some(factors) //we know the prime factorization for sure
-        }
-    };
-    None // n is larger than our largest known prime. it could be prime, but it could also be a sufficiently large composite number
-}
-
