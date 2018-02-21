3baf360f77fc9e10a94fa409a3e783bd36960f4d
efron_licht <efron.python@gmail.com>
2018-02-21 11:48:53 -0800

    changed Primes to newtype; added constructor method

--- number_theory/src/primes.rs
+++ number_theory/src/primes.rs
@@ -1,92 +1,121 @@
 use std::collections;
-pub type Counter = collections::HashMap<u64, u32>;
-pub type Primes = Vec<u64>;
 
-pub fn union(a: &Counter, b: &Counter) -> Counter {
+///factors represents the prime factors of an integer. the key is the base, and the value is the exponent.
+///we note that since (1<<64-1) < (2<<64) is the max u64, the maximum possible exponent we can store without going into 
+///variable-precision arithmetic is 62. 
+pub type Factors = collections::HashMap<u64, u8>;
+
+#[derive(Debug, Clone, PartialEq)]
+///Primes represents a subset of the primes, starting with no gaps from the beginning of the positive integers, under a certain maximum.
+///Valid Primes are [], [2, 3, 5], [2, 3, 5, 7, 11], but NOT [2, 5, 11].
+pub struct Primes(Vec<u64>);
+
+impl Primes {
+    ///the maximum prime in the reperesentation
+    pub fn max(&self) -> Option<u64> {
+        if self.0.len() == 0 {
+            None
+        } else {
+            Some(self.0[self.0.len()-1])
+        }
+    }
+
+    ///create a Primes representing all primes under 'max' using the sieve of Eratosthenes.
+    pub fn under(max: u64) -> Self {
+        let mut primes = Primes(Vec::new());
+        if max < 2 {
+            return primes;
+        }
+        primes.0.push(2);
+        let mut n = 3;
+
+        while n < max {
+            let mut isprime = true;
+            for p in &primes.0 {
+                if n % p == 0 {
+                    isprime = false;
+                    break;
+                }
+            }
+            if isprime {
+                primes.0.push(n);
+            }
+
+            n += 2
+        }
+        primes
+    }
+    
+    ///convert a Vec<64> into Primes directly, without checking that they are actually prime.
+    pub unsafe fn from_raw_vec(v: Vec<u64>) -> Self {Primes(v)}
+}
+
+impl Into<Vec<u64>> for Primes {fn into(self) -> Vec<u64> {self.0}}
+
+pub fn union(a: &Factors, b: &Factors) -> Factors {
     let mut union = a.clone();
     for (k, v) in b {
-         if !union.contains_key(k) {
-                union.insert(*k, *v);
-            } else {
-                let m = union[k];
-                union.insert(*k, m.max(*v));
+        if !union.contains_key(k) {
+            union.insert(*k, *v);
+        } else {
+            let m = union[k];
+            union.insert(*k, m.max(*v));
         }
     }
     union
 }
 
-
-pub fn intersection(a: &Counter, b: &Counter) -> Counter {
-    let mut intersection = Counter::new();
+pub fn intersection(a: &Factors, b: &Factors) -> Factors {
+    let mut intersection = Factors::new();
     for (k, v) in a {
         if b.contains_key(k) {
-            let min = *v.min(&b[k]);
-            intersection.insert(*k, min);
+            let min = v.min(&b[k]);
+            intersection.insert(*k, *min);
         }
     }
     intersection
 }
 ///gcd is the greatest common divisor.  that is, the largest d such that ad == m bd == n for some positive integers a, b
 pub fn gcd(m: u64, n: u64, primes: &Primes) -> Option<u64> {
-    if m == 0 || n == 0 {return None}
+    if m == 0 || n == 0 {
+        return None;
+    }
     let intersection = intersection(&factors(n, primes)?, &factors(m, primes)?);
     let mut gcd = 1;
     for (p, exp) in intersection {
-        gcd *= p.pow(exp);
+        gcd *= p.pow(exp as u32);
     }
     Some(gcd)
 }
 
-//lcm is the least common multiple; that is, the smallest q such that a*m == q, b*m == q for some positive integers a, b
+//lcm is the least common multiple; that is, the smallest q such that a*m == q, b*n == q for some positive integers a, b
 pub fn lcm(m: u64, n: u64, primes: &Primes) -> Option<u64> {
-    if m == 0 || n == 0 {
-        return Some(0)
-    }
-    let union = union(&factors(n, primes)?, &factors(m, primes)?);
-    let mut lcm = 1;
-    for (p, exp) in union.iter() {
-        lcm *= p.pow(*exp);
-    }
-    Some(lcm)
-}
-
-//find all primes under max, returning a Vec<u64>. this is not a particularly efficient implementation.
-pub fn primes_under(max: u64) -> Primes {
-    let mut primes = Primes::new();
-    if max < 2 {
-        return primes    
-    }
-    primes.push(2);
-    let mut n = 3;
-
-
-    while n < max{
-        let mut isprime = true;
-        for p in &primes {
-            if n % p == 0 {
-                isprime = false;
-                break 
+    match (m, n) {
+        (0, 0) => Some(0), 
+        (0, _) | (_, 0) => None, 
+        (m, n) => 
+        {
+            let union = union(&factors(n, primes)?, &factors(m, primes)?);
+            let mut lcm = 1;
+            for (p, exp) in union {
+                lcm *= p.pow(exp as u32);
             }
+            Some(lcm)
         }
-        if isprime {
-            primes.push(n);
-        }
-
-        n +=2
     }
-    primes
 }
+
 
 
 ///find the prime factors of a positive integer, if any. 0 and 1 are considered to have no prime factorization.
-pub fn factors(n: u64, primes: &Primes) -> Option<Counter>{
+pub fn factors(n: u64, primes: &Primes) -> Option<Factors> {
     if n == 0 || n == 1 {
-        return None
+        return None;
     }
-    let mut factors = Counter::new();
+    let mut factors = Factors::new();
     let mut n = n;
 
-    for p in primes {
+    for p in &primes.0 {
         let p = *p;
         while n % p == 0 {
             *factors.entry(p).or_insert(0) += 1;
@@ -95,7 +124,6 @@ pub fn factors(n: u64, primes: &Primes) -> Option<Counter>{
         if p > n {
             return Some(factors); //we know the prime factorization for sure
         }
-    };
+    }
     None // n is larger than our largest known prime. it could be prime, but it could also be a sufficiently large composite number
 }
-
--- number_theory/src/tests.rs
+++ number_theory/src/tests.rs
@@ -1,26 +1,33 @@
 use super::primes::*;
-
 #[test]
 fn test_primes_under() {
-    let p = primes_under(12);
-    assert_eq!(vec![2, 3, 5, 7, 11], p);
+    let got = Primes::under(12);
+    let want = unsafe{Primes::from_raw_vec(vec![2, 3, 5, 7, 11])};
+    assert_eq!(want, got);
 }
 
 #[test]
 fn test_factors() {
-    let want = Some(hashmap! {2 => 3, 3 => 1});
-    assert_eq!(want, factors(8*3, &primes_under(12)));
+    let got = factors(8 * 3, &Primes::under(12));
+    let want = Some(hashmap!{2 => 3, 3 => 1});
+    assert_eq!(want, got);
 }
 
 #[test]
-fn test_gcd() { // tests intersection implicitly
-    let got = gcd(2*3*5, 2*2*2*3, &primes_under(12)).unwrap();
-    assert_eq!(got, 2*3);
-}
+fn test_gcd() {
+    // tests intersection implicitly
+    let p = Primes::under(12);
+    let want =  Some(2 * 3 as u64);
+    let got = gcd(2 * 3 * 5, 2 * 2 * 2 * 3, &p);
+    assert_eq!(want, got);
 
+    let got = gcd(23, 2, &p); // fail due to primes not being big enough
+    assert_eq!(None, got)
+}
 
 #[test]
 fn test_lcm() {
-    let got = lcm(2*3, 5*2, &primes_under(12)).unwrap();
-    assert_eq!(got, 2*3*5);
+    let want = Some(2 * 3 * 5 as u64);
+    let got = lcm(2 * 3, 5 * 2, &Primes::under(12));
+    assert_eq!(want, got);
 }
